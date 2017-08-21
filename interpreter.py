3246734cf1b26acf761f3363190c4e16482feea5
Sharan Srivatsa <sharans003@gmail.com>
2017-08-21 08:02:26 -0600

    Some corrections

--- interpreter.py
+++ interpreter.py
@@ -32,14 +32,14 @@ class Interpreter(object):
 		self.operator_list = []
 		## First assuming that all the lex tokens are space separated
 		self.lex_list = self.text.split() # Lexer class
-		#print "leeex " + str(self.lex_list)
+		
 		 
 
 	def handle_print_statements(self):
 		#print "HANDLING PRINT STATEMENTS"
-		print variable_map
+		#print variable_map
 		expression_val = self.evaluate_expression()
-		#print "FINAL RES " + str(expression_val)
+		print expression_val
 
 
 	def handle_assignment_statements (self, temp_pos):
@@ -49,24 +49,23 @@ class Interpreter(object):
 		variable_map[self.lex_list[temp_pos]] = expression_val
 		
 	def evaluate(self):
-		print self.pos
+		#print self.pos
 
 		if self.lex_list[self.pos] == "print":
 			self.is_print = 1
 			self.pos = self.pos + 1
 			self.handle_print_statements ()
 
-		elif self.lex_list[self.pos].isalpha():
+		elif self.lex_list[self.pos].isalpha() and self.lex_list[self.pos+1] == '=':
 			## this seems to be an assignment operation
 			if self.lex_list[self.pos] not in variable_map:
 				variable_map[self.lex_list[self.pos]] = None
 
 			temp_pos = self.pos
-			print "BOSS " + str(variable_map)
+			
 			self.pos = self.pos + 2
 			self.handle_assignment_statements (temp_pos)
-			print "BOSS 1 " + str(variable_map)
-			## to confirm if this is an assignment operation
+			
 
 	def clear_lists (self):
 		self.operator_list = []
@@ -99,16 +98,14 @@ class Interpreter(object):
 					return
 				else:
 					var = self.lex_list[self.pos]
-					print str(var)
-					print "VARRRRR " + str(variable_map[var])
-					self.lex_list[self.pos] = variable_map[var]
+					self.lex_list[self.pos] = str(variable_map[var])
 
 
 			# for the first operator and no operand
 			if self.lex_list[self.pos].isdigit():
 				if 	(len(self.operator_list) == 0):
 					self.operand_list.append(int(self.lex_list[self.pos]))
-					print self.operand_list
+					#print self.operand_list
 				elif ((self.operator_list[-1] == '+')):
 					self.operand_list.append(int(self.lex_list[self.pos]))
 				# on encountering the multiplication operator pop the first 2 elements from the operand list and the * or / operator
@@ -116,7 +113,7 @@ class Interpreter(object):
 					self.operand_list.append (int(self.lex_list[self.pos]) * self.operand_list.pop())
 					self.operator_list.pop()
 				elif (self.operator_list[-1] == '/'):
-					print self.lex_list[self.pos] + "POPPED ELEMENT " + str(self.operand_list[-1])
+					#print self.lex_list[self.pos] + "POPPED ELEMENT " + str(self.operand_list[-1])
 					self.operand_list.append (self.operand_list.pop() / int(self.lex_list[self.pos]))
 					self.operator_list.pop()
 			
@@ -132,8 +129,8 @@ class Interpreter(object):
 
 
 
-		print "OPERATOR LIST " + str(self.operator_list) + "\n"
-		print "OPERAND LIST " + str(self.operand_list) + "\n"
+		#print "OPERATOR LIST " + str(self.operator_list) + "\n"
+		#print "OPERAND LIST " + str(self.operand_list) + "\n"
 
 		'''
 				Till now all the multiplication and division operations are done. Now we need to descend through the stack and complete
